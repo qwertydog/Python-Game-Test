b9686850d0ab339a0f56069a51844955b42f3d55
qwertydog <campbelldeas@hotmail.com>
2015-12-15 18:36:08 +1300

    Player can move left and right using arrow keys

--- PyGame/PyGame.py
+++ PyGame/PyGame.py
@@ -5,49 +5,47 @@ from pygame.locals import *
 def process_events():
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or \
-            (event.type == KEYDOWN and event.key == K_ESCAPE):
-            
+        if event.type == pygame.QUIT:            
             return False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-
             position = pygame.mouse.get_pos()
             message = "Mouse clicked at position: " + str(position)
             text = font.render(message, 1, (255, 0, 0))
-            display.fill(Color("black"))
-            player.draw()
-            display.blit(text, position)
-            
+            display.fill(Color("black"))        
+            display.blit(text, position)    
 
         elif event.type == pygame.MOUSEBUTTONUP:
             pass
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                pass
+                player.jump()
 
             elif event.key == pygame.K_DOWN:
-                pass
+                player.crouch()
 
             elif event.key == pygame.K_LEFT:
-                pass
+                player.move(Direction.left)
 
             elif event.key == pygame.K_RIGHT:
-                pass
+                player.move(Direction.right)
+
+            elif event.key == pygame.K_SPACE:
+                player.shoot()
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
-                pass
+                player.unjump()
 
             elif event.key == pygame.K_DOWN:
-                pass
+                player.stand()
 
             elif event.key == pygame.K_LEFT:
-                pass
+                player.stop(Direction.left)
 
             elif event.key == pygame.K_RIGHT:
-                pass
+                player.stop(Direction.right)
 
     return True
 
@@ -79,28 +77,100 @@ class Direction(enum.Enum):
     right = 1
     left = 2
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, spritesheet, position=[0,0], velocity=0, direction = Direction.right):
-        pygame.sprite.Sprite.__init__(self)
+class Animation():
+    def __init__(self, sprites_list):
+        self.sprites = sprites_list
+        self.num_sprites = len(sprites)
 
+class Player(pygame.sprite.Sprite):
+    velocity = [0, 0]
+    state = "still"
+    max_velocity = 10
+    max_jump_height = 50
+
+    def __init__(self, spritesheet, position=[0,0], direction = Direction.right):
+        pygame.sprite.Sprite.__init__(self)
+        
         self.spritesheet = spritesheet
+        #self.animation_dict = animation_dict
+        self.image = get_sprite_from_spritesheet(spritesheet, Rect(0, 64*8, 64, 64))
+        self.rect = self.image.get_rect()
         self.position = position
+        self.ground_level = self.rect.bottom
         self.direction = direction
-        self.velocity = velocity
+        
+    def update(self):
+        if self.state == "still":
+            pass
+        elif self.state == "moving":
+            self.move(self.direction)
+        elif self.state == "jumping":
+            self.jump()
+        elif self.state == "crouching":
+            self.crouch()
 
     def move(self, direction = Direction.right):
-        self.position[0] += 1
-        self.direction  = direction
-        self.velocity = 1
+        
+       # if self.rect.bottom == self.ground_level:
+        if self.state != "moving":
+            self.state = "moving"
+
+       #Anima
+       
+       
+
+        if direction != self.direction:
+            self.image = pygame.transform.flip(self.image, 1, 0)
+            self.direction = direction
+
+        if direction == Direction.right:
+            self.position[0] += self.velocity[0]
+        else:
+            self.position[0] -= self.velocity[0]
+
+        if self.velocity[0] < self.max_velocity:
+            self.velocity[0] += 1
+
+    def stop(self, direction = Direction.right):
+        if self.direction == direction:
+            self.state = "still"
+            self.velocity[0] = 0
+
+    def jump(self):
+        """self.state = "jumping"
+
+        if self.velocity[1] > self.max_jump_height:
+            self.velocity[1] = self.max_jump_height
+        else:
+            self.velocity[1] += 2
+
+        if self.image.get_rect().bottom == self.ground_level:
+            self.velocity[1] = -5
+
+        if self.direction == Direction.right:
+            self.position[0] += self.velocity[0]
+        else:
+            self.position[0] -= self.velocity[0]
+
+        self.position[1] += self.velocity[1]"""
+        pass
+
+    def unjump(self):
+        #self.state = "still"
+        pass
+
+    def crouch(self):
+        pass
+
+    def stand(self):
+        pass    
 
     def draw(self):
-        self.sprite = get_sprite_from_spritesheet(self.spritesheet)
-        display.blit(self.sprite, tuple(self.position)) 
+        display.blit(self.image, tuple(self.position)) 
 
 
-class Spritesheet(pygame.sprite.Sprite):
+class Spritesheet():
     def __init__(self, spritesheet):
-        pygame.sprite.Sprite.__init__(self)
         
         self.sprite = load_image(spritesheet)
         self.rect = self.sprite.get_rect()
@@ -109,7 +179,7 @@ class Spritesheet(pygame.sprite.Sprite):
 
 pygame.init()
 
-resolution = width, height = (1280, 960)
+resolution = width, height = (1280, 800)
 
 FPS = 60
 
@@ -131,14 +201,16 @@ player_spritesheet = Spritesheet("platformerSprites.png")
 
 player = Player(player_spritesheet, [500, 500])
 
+ground_rect = Rect(0, 500+64, width, 500-64)
 
 while process_events():
 
     """Drawing calls"""            
 
-    #display.fill((0, 0, 0))
-
-    #player.draw()
+    display.fill(BLACK)
+    display.fill((0, 130, 0), ground_rect)
+    player.update()
+    player.draw()
 
     pygame.display.flip()
 
